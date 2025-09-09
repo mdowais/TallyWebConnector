bf6ab7892631da251455c63a454704dd2cbf40b4
Mohammed Owais <owais@patronum.org>
2025-09-09 22:04:33 +0530

    first commit

---
+++ Controllers/LicenseController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TallyConnector.Core.Models;
+using TallyConnector.Services;
+
+
+namespace TallyWebConnector.Controllers
+{
+    [Route("core")]
+    [ApiController]
+    public class CoreController : ControllerBase
+    {
+        private readonly IConfiguration config;
+        private TallyService tally;
+        public CoreController(IConfiguration configuration)
+        {
+            config = configuration;
+            tally = new TallyService(config.GetSection("TallyHost").Value, int.Parse(config.GetSection("TallyPort").Value));
+        }
+
+        [HttpGet("check")]
+        public async Task<bool> check()
+        {
+            return await tally.CheckAsync(); 
+        }
+
+        // GET: api/<LicenseController>
+        [HttpGet("license")]
+        public object license()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<LicenseController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<LicenseController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<LicenseController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<LicenseController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
